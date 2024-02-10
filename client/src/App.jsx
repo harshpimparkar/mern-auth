6ba4a0114afa28f13c20ae19c323fbefcc99b561
Harsh <harshypimparkar@gmail.com>
2024-02-10 17:41:49 +0530

    Add routes and links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      About
+      <h1>Auth app</h1>
+      <h3>Beta v0.1</h3>
     </div>
   )
 }
